cfbb4268742c4c3f595c8a012d09b7b1463ccdc2
DenysIlnytskyi <380984511271k@gmail.com>
2022-01-21 20:13:45 +0100

    Final

--- Homework2.3/ViewController.swift
+++ Homework2.3/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginTextField: UITextField!
+    
+    private let login = "User"
+    private let password = "Password"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcome = segue.destination as? WelcomeViewController else { return }
+        welcome.user = login
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        loginTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    @IBAction func helpRememberLogin() {
+        alert(title: "Oops!", message: "Your login is User😉", style: .alert)
+    }
+    
+    
+    @IBAction func helpRememberPassword() {
+        alert(title: "Oops!", message: "Your password is Password😉", style: .alert)
+    }
+    
+    @IBAction func logIn() {
+        if loginTextField.text != login || passwordTextField.text != password {
+            alert(title: "Invalid Login or Password", message: "Please, enter correct Login or Password", style: .alert)
+            passwordTextField.text = ""
+        }
+    }
+    
+    
+    
+    private func alert(title: String, message: String, style: UIAlertController.Style) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "Ok", style: .default)
+        
+        alert.addAction(action)
+        present(alert, animated: true)
 }
-
+}
---
+++ Homework2.3/WelcomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeViewController.swift
+//  Homework2.3
+//
+//  Created by Денис Ільницький on 21/01/2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet var welcomeUser: UILabel!
+    
+    var user = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeUser.text = "Welcome, \(user)!"
+       
+    }
+    
+
+    
+}
